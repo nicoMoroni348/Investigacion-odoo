9cb03a60103f11816889c1d02f028eed30c0de85
nico <nicolasmoroni003@gmail.com>
2024-04-03 16:46:57 -0300

    primer addon básico, sin funcionalidad

---
+++ addon_prueba/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'addon_prueba',
+    'version': '1.0.0',
+    'category': 'Sin Categoría',
+    'summary': 'Este es mi primer addon',
+    'description': 'Este es mi primer addon',
+    'depends': [],  # Por ahora no depende de ningún módulo externo
+    'data': [],  # Por ahora no necesita ningun archivo EXTERNO
+    # (acordate que los internos van en el __init__)
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',  # Esta es la que viene por defecto
+
+}
