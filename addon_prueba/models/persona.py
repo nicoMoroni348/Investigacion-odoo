d6d74d6ea82cb551d593e66b1191e3b8885a6865
nico <nicolasmoroni003@gmail.com>
2024-04-05 16:20:55 -0300

    Primera Action

---
+++ addon_prueba/models/persona.py
@@ -0,0 +1,9 @@
+from odoo import api, fields, models
+
+class Persona(models.Model):
+    _name = 'persona'
+    _description = 'Este es mi primer modelo'
+
+    name = fields.Char(string='Nombre', required=True)
+    age = fields.Integer(string='Edad', required=False)
+    gender = fields.Selection([('Hombre', 'hombre'), ('Mujer', 'mujer'), ('Otro', 'otro')], string='Genero')
