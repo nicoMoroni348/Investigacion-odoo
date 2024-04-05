37f500f07af653b9b6df4ead0b8378d1ab4347e4
nico <nicolasmoroni003@gmail.com>
2024-04-05 16:20:38 -0300

    Primera Action

--- addon_prueba/__manifest__.py
+++ addon_prueba/__manifest__.py
@@ -2,13 +2,16 @@
     'name': 'addon_prueba',
     'version': '1.0.0',
     'category': 'Sin Categoría',
-    'sequence': -100,
+    'sequence': -1500, # así aparece primero, también pone lo del application true
     'author': 'nico',
+    'application': True,
     'summary': 'Este es mi primer addon',
     'description': 'Este es mi primer addon',
     'depends': [],  # Por ahora no depende de ningún módulo externo
-    'data': [],  # Por ahora no necesita ningun archivo EXTERNO
-    # (acordate que los internos van en el __init__)
+    'data': [
+        'views/menu.xml',
+        'views/persona_view.xml',
+    ],
     'demo': [],
     'installable': True,
     'auto_install': False,
