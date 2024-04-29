91228eb69ed7adac00b1898b031a7e8fbef1ec5e
nico <nicolasmoroni003@gmail.com>
2024-04-28 21:51:27 -0300

    computed field

--- addon_prueba/models/persona.py
+++ addon_prueba/models/persona.py
@@ -1,4 +1,5 @@
 from odoo import api, fields, models
+from datetime import date
 
 
 class Persona(models.Model):
@@ -10,21 +11,29 @@ class Persona(models.Model):
     _description = 'Este es mi primer modelo'
 
     name = fields.Char(string='Nombre', required=True, tracking=True)
-    age = fields.Integer(string='Edad', required=False)
+    fecha_nacimiento = fields.Date(string="Fecha de nacimiento")
+    age = fields.Integer(string='Edad', compute='_compute_age', store=True) # campo que se calcula, y se guarda en la bda porque tiene el store=True
     gender = fields.Selection([('Hombre', 'hombre'), ('Mujer', 'mujer'), ('Otro', 'otro')], string='Genero',
                               required=True)
     active = fields.Boolean(string="Activo", default=True)
+    email = fields.Char(string='Correo Electrónico', required=True)
 
-    # """ ESTO ME LO TIRO EL CHAT PARA SOLUCIONAR EL ERROR DEL CHATTER"""
+    @api.depends('fecha_nacimiento')
+    def _compute_age(self):
+        for rec in self: # para evitar el SINGLETON error
+            hoy = date.today()
+            if rec.fecha_nacimiento:
+                rec.age = hoy.year - rec.fecha_nacimiento.year
+            else:
+                rec.age = 0
 
-    email = fields.Char(string='Correo Electrónico')
+    @api.depends('email')
+    def _compute_email_normalized(self):
+        for record in self:
+            if record.email:
+                # Realiza la normalización del correo electrónico aquí
+                normalized_email = record.email.lower()  # Por ejemplo, convierte a minúsculas
+                record.email_normalized = normalized_email
+            else:
+                record.email_normalized = False
 
-@api.depends('email')
-def _compute_email_normalized(self):
-    for record in self:
-        if record.email:
-            # Realiza la normalización del correo electrónico aquí
-            normalized_email = record.email.lower()  # Por ejemplo, convierte a minúsculas
-            record.email_normalized = normalized_email
-        else:
-            record.email_normalized = False
